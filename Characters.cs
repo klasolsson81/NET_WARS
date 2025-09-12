e94f541d060d3420bf212656742cdd72431bf067
klasolsson81 <klasolsson81@gmail.com>
2025-09-12 11:59:19 +0200

    Uppdaterade Program.CS La till klassen Characters

---
+++ Characters.cs
@@ -0,0 +1,21 @@
+﻿namespace NET_WARS
+{
+    internal class Characters
+    {
+        //Här skapar jag en klass som heter Characters
+        //Klasser innehåller egenskaper och metoder
+
+        //Egenskaper
+        public string Name;
+        public int Health;
+        public int AttackPower;
+
+        //Metoder
+        public void Damage(int damage)
+        {
+            Health = Health - damage;
+            Console.WriteLine($"{Name} har tagit {damage} skada och har nu {Health} HP kvar.");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace NET_WARS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Här börjar programmet
+            //Inlämningsuppgift: 
+            //✅ Skapa en Console Application i C#
+            //✅ I projektet ska du ha fyra olika klasser/ filer – en för varje OOP-princip:
+            //* Arv
+            //* Polymorfism
+            //* Inkapsling
+            //* Abstraktion
+
+            // Nu ska jag anropa min första klass som heter Characters
+            Characters hero = new Characters();
+            hero.Name = "The Beast";
+            hero.Health = 100;
+            hero.AttackPower = 20;
+            hero.Damage(30);
+
+            //Nästa klass är Enemy som ärver från Characters
+
+
+        }
+    }
+}
